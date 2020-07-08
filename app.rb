c0bca8da3867f963ffd0299ac993196e8f875382
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-07-07 20:47:10 -0700

    Done.

--- app.rb
+++ app.rb
@@ -22,4 +22,34 @@ class App < Sinatra::Base
     @phrase * @num
   end
 
+  get "/say/:word1/:word2/:word3/:word4/:word5" do 
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+
+    if @operation == 'add'
+      @sum = @num1 + @num2
+      @sum.to_s
+    elsif @operation == 'subtract'
+      @subt = @num1 - @num2
+      @subt.to_s
+    elsif @operation == 'multiply'
+      @multi = @num1 * @num2
+      @multi.to_s
+    elsif @operation == 'divide'
+      @div = @num1 / @num2
+      @div.to_s
+    end
+  end
+
+
 end
