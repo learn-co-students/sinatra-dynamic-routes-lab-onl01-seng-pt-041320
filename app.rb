2d6447f70dbd380ca9efbedad95a5857c8ca07a8
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-07-07 19:48:10 -0700

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,20 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get "/reversename/:name" do 
+    @name = params[:name].to_s
+    @rev = @name.reverse
+    "#{@rev}"
+  end
+
+  get "/square/:number" do
+    @num = params[:number]
+    @x = @num.to_i
+    @square = @x**2
+    "#{@square}"
+  end
+
+  get "/say/:number/:phrase" do 
+  end
+
 end
