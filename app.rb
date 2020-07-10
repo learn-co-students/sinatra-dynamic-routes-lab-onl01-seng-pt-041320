063dc285c5bf72c9b09dd39abd47c8ba7cf75ad8
dnodolf1212 <dnodolf@gmail.com>
2020-07-10 01:51:14 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,51 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get '/reversename/:name' do
+    @name = params[:name]
+    @name.reverse
+  end 
+
+  get '/square/:number' do
+    "#{params[:number].to_i * params[:number].to_i}"
+  end
+
+  get '/say/:number/:phrase' do
+    str = ""
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    @num.times { str += "#{@phrase}\n" }
+    str
+    #@phrase = params[:phrase]
+    #params[:number].times do
+      #"#{@phrase}"
+  end 
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} " + "#{params[:word2]} " + "#{params[:word3]} " + "#{params[:word4]} " + "#{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    num1 = params[:number1].to_i 
+    num2 = params[:number2].to_i
+
+    if @operation == "add"
+      @result = (num1 + num2).to_s
+    elsif
+      @operation == "subtract"
+      @result = (num1 - num2).to_s
+    elsif
+      @operation == "multiply"
+      @result = (num1 * num2).to_s
+    elsif
+      @operation == "divide"
+      @result = (num1 / num2).to_s
+    end
+    @result
+  end 
+
+
 
 end
